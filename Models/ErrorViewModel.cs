b383a08adb94892f219ab426ddd085f842d98974
Robbie-kress <68262512+Robbie-kress@users.noreply.github.com>
2020-08-24 10:27:38 -0400

    movie lab

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Movies_Model_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
